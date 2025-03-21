4df195ce5ddf48eb1e18dc733bdf0972e324cd6f
Emma LIEFMANN <emmaliefmann@gmail.com>
2025-03-21 12:18:26 +0100

    Creation packages, fonction topics complète

--- back/src/main/java/com/openclassrooms/mddapi/config/SwaggerConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/config/SwaggerConfig.java
@@ -1,15 +1,5 @@
 package com.openclassrooms.mddapi.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@OpenAPIDefinition(
-        info =@Info(
-                title = "Orion - Monde du Dev (MDD)",
-                description = "Project 6 for Fullstack developer degree, created by Emma Liefmann"
-        )
-)
 public class SwaggerConfig {
 }
